08de022157cf338827bc7fabe56e86444d887f26
Nolan Lawson <nlawson@salesforce.com>
2021-10-12 14:02:45 -0700

    fix: recover from errors due to relying on globals

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ const { transformSync } = require('@babel/core')
 const { cloneNode } = require('@babel/types')
 
 const kagekiriSourceFilename = require.resolve('kagekiri/dist/kagekiri.iife.min.js')
-// convert IIFE to something that always creates a global window.__kagekiri__ object
 const kagekiriSource = fs.readFileSync(kagekiriSourceFilename, 'utf8')
-  .replace('var kagekiri', 'window.__kagekiri__')
+// convert IIFE to something that always creates a global window.__kagekiri__ object
+const kagekiriSourceAsGlobal = kagekiriSource.replace('var kagekiri', 'window.__kagekiri__')
+// same thing, but keep it as an inline var, just rename kagekiri to __kagekiri__
+const kagekiriSourceAsInline = kagekiriSource.replace('var kagekiri', 'var __kagekiri__')
 
-function patchCodeToUseKagekiri (inputCode) {
+function patchCodeToUseKagekiri (inputCode, hasKagekiriAsGlobal) {
   const propertyNames = ['querySelector', 'querySelectorAll']
   try {
-    const { code } = transformSync(inputCode, {
+    let { code } = transformSync(inputCode, {
       parserOpts: {
         allowReturnOutsideFunction: true
       },
@@ -39,6 +41,16 @@ function patchCodeToUseKagekiri (inputCode) {
         }
       }]
     })
+    if (hasKagekiriAsGlobal) {
+      // Add a check for the kagekiri global, which we can catch later if necessary
+      const globalCheck = 'if (typeof __kagekiri__ === "undefined") { throw new Error("__kagekiri__ is undefined") }'
+      code = `${globalCheck}\n${code}`
+    } else {
+      // If we were unable to load kagekiri as a global (i.e. `window.__kagekiri__`), then fall back to injecting
+      // the entire kagekiri source along with the code. This is more inefficient (since the entire kagekiri source
+      // has to be included in every call), but at least it works.
+      code = `${kagekiriSourceAsInline}\n${code}`
+    }
     return code
   } catch (err) {
     // This can happen if the input is not JavaScript, e.g. Chromedriver-specific commands like `:takeHeapSnapshot`.
@@ -88,32 +100,44 @@ function shimShadowDom (browser) {
       // If overriding client.executeAll(), then calling client.execute() is fine.
       const nonAsyncExecuteFunction = isAsync ? browser.execute : origFunction
 
-      function checkHasKagekiri () {
+      function checkHasKagekiriAsGlobal () {
         return nonAsyncExecuteFunction.call(browser, 'return !!window.__kagekiri__')
       }
 
-      function loadAndCheckHasKagekiri () {
+      function loadAndCheckHasKagekiriAsGlobal () {
         return nonAsyncExecuteFunction.call(
           browser,
-          `return (function () { ${kagekiriSource}; return !!window.__kagekiri__; })()`
+          `return (function () { ${kagekiriSourceAsGlobal}; return !!window.__kagekiri__; })()`
         )
       }
 
-      async function loadKagekiriIfNecessary () {
-        if (!(await checkHasKagekiri())) {
-          const hasKagekiri = await loadAndCheckHasKagekiri()
-          if (!hasKagekiri) {
-            throw new Error('unable to load kagekiri')
-          }
+      async function tryToLoadKagekiriAsGlobal () {
+        if (await checkHasKagekiriAsGlobal()) {
+          return true
         }
+        return (await loadAndCheckHasKagekiriAsGlobal())
       }
 
-      await loadKagekiriIfNecessary()
+      const hasKagekiriAsGlobal = await tryToLoadKagekiriAsGlobal()
 
       // If it's a string, webdriverio treats it as just the function body
       const scriptAsString = typeof script === 'function' ? `return (${script}).apply(null, arguments)` : script
-      const newScript = patchCodeToUseKagekiri(scriptAsString)
-      return origFunction.call(browser, newScript, ...args)
+      const newScript = patchCodeToUseKagekiri(scriptAsString, hasKagekiriAsGlobal)
+      try {
+        return (await origFunction.call(browser, newScript, ...args))
+      } catch (err) {
+        // Check if this is an error due to window.__kagekiri__ not being available.
+        // This can happen if the page happens to redirect between when we write window.__kagekiri__ and
+        // when we try to read window.__kagekiri__.
+        const kagekiriGlobalError = hasKagekiriAsGlobal && err.message.includes('__kagekiri__ is undefined')
+        if (kagekiriGlobalError) {
+          // Recover by just inlining kagekiri instead of relying on the global
+          const newScriptAsInline = patchCodeToUseKagekiri(scriptAsString, false)
+          return (await origFunction.call(browser, newScriptAsInline, ...args))
+        } else {
+          throw err
+        }
+      }
     }
   }
 
--- test/spec/plugin.test.js
+++ test/spec/plugin.test.js
@@ -5,142 +5,176 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 /* global describe it beforeEach browser $ $$ */
-const { expect } = require('chai')
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+const { expect } = chai
 
 describe('plugin', () => {
-  beforeEach(async () => {
-    await browser.url('/shadowdom.html')
-  })
+  describe('basic', () => {
+    beforeEach(async () => {
+      await browser.url('/shadowdom.html')
+    })
 
-  it('can pierce the shadow dom', async () => {
-    const element = await $('.text')
-    expect(element.error).to.equal(undefined)
-    expect(element.selector).to.equal('.text')
-    expect(element.parent).to.equal(browser)
-    await element.waitForExist()
-    const text = await element.getText()
-    expect(text).to.equal('Hello from the shadow DOM')
-  })
+    it('can pierce the shadow dom', async () => {
+      const element = await $('.text')
+      expect(element.error).to.equal(undefined)
+      expect(element.selector).to.equal('.text')
+      expect(element.parent).to.equal(browser)
+      await element.waitForExist()
+      const text = await element.getText()
+      expect(text).to.equal('Hello from the shadow DOM')
+    })
 
-  it('can get shadow elements the old-fashioned way, without the plugin', async () => {
-    await (await $('.done')).waitForExist()
-    const element = await (await $('custom-component')).shadow$('.text')
-    expect(element.error).to.equal(undefined)
-    const text = await element.getText()
-    expect(text).to.equal('Hello from the shadow DOM')
-  })
+    it('can get shadow elements the old-fashioned way, without the plugin', async () => {
+      await (await $('.done')).waitForExist()
+      const element = await (await $('custom-component')).shadow$('.text')
+      expect(element.error).to.equal(undefined)
+      const text = await element.getText()
+      expect(text).to.equal('Hello from the shadow DOM')
+    })
 
-  it('can click buttons in the shadow dom', async () => {
-    await (await $('.btn')).click()
-    expect(await (await $('.text')).getText()).to.equal('I was changed!')
-  })
+    it('can click buttons in the shadow dom', async () => {
+      await (await $('.btn')).click()
+      expect(await (await $('.text')).getText()).to.equal('I was changed!')
+    })
 
-  it('can use $$', async () => {
-    const selector = 'custom-component, .btn, .text'
-    const elements = await $$(selector)
-    expect(elements.selector).to.equal(selector)
-    expect(elements.parent).to.equal(browser)
-    expect(elements.foundWith).to.equal('$$')
-    expect(elements.map(_ => _.selector)).to.deep.equal(elements.map(() => selector))
-    expect(elements.map(_ => _.index)).to.deep.equal([0, 1, 2])
-    expect(await Promise.all(elements.map(_ => _.getAttribute('class'))))
-      .to.deep.equal(['custom', 'text', 'btn'])
-  })
+    it('can use $$', async () => {
+      const selector = 'custom-component, .btn, .text'
+      const elements = await $$(selector)
+      expect(elements.selector).to.equal(selector)
+      expect(elements.parent).to.equal(browser)
+      expect(elements.foundWith).to.equal('$$')
+      expect(elements.map(_ => _.selector)).to.deep.equal(elements.map(() => selector))
+      expect(elements.map(_ => _.index)).to.deep.equal([0, 1, 2])
+      expect(await Promise.all(elements.map(_ => _.getAttribute('class'))))
+        .to.deep.equal(['custom', 'text', 'btn'])
+    })
 
-  it('can use findElement() and findElements()', async () => {
-    const selector = '.text'
-    const element = await browser.findElement('css selector', selector)
-    expect(element.selector).to.equal(selector)
-    expect(await element.getText()).to.equal('Hello from the shadow DOM')
-    const elements = await browser.findElements('css selector', selector)
-    expect(elements.selector).to.equal(selector)
-    expect(elements[0].selector).to.equal(selector)
-    expect(await elements[0].getText()).to.equal('Hello from the shadow DOM')
-  })
+    it('can use findElement() and findElements()', async () => {
+      const selector = '.text'
+      const element = await browser.findElement('css selector', selector)
+      expect(element.selector).to.equal(selector)
+      expect(await element.getText()).to.equal('Hello from the shadow DOM')
+      const elements = await browser.findElements('css selector', selector)
+      expect(elements.selector).to.equal(selector)
+      expect(elements[0].selector).to.equal(selector)
+      expect(await elements[0].getText()).to.equal('Hello from the shadow DOM')
+    })
 
-  it('can use execute()', async () => {
-    expect(await browser.execute(
-      function () {
-        return document.querySelector('.text').innerText
+    it('can use execute()', async () => {
+      expect(await browser.execute(
+        function () {
+          return document.querySelector('.text').innerText
+        }
+      )).to.equal('Hello from the shadow DOM')
+      expect(await
+      browser.execute('return document.querySelector(\'.text\').innerText;')
+      ).to.equal('Hello from the shadow DOM')
+      expect(await browser.execute(
+        function (selector) {
+          return document.querySelector(selector).innerText
+        },
+        '.text'
+      )).to.equal('Hello from the shadow DOM')
+      expect(await browser.execute(
+        'return document.querySelector(arguments[0]).innerText;',
+        '.text'
+      )).to.equal('Hello from the shadow DOM')
+      expect(await browser.execute(
+        () => document.querySelector('.text').innerText
+      )).to.equal('Hello from the shadow DOM')
+      expect(await browser.execute(
+        selector => document.querySelector(selector).innerText,
+        '.text'
+      )).to.equal('Hello from the shadow DOM')
+    })
+
+    it('can use executeAsync()', async () => {
+      expect(await browser.executeAsync(function (done) {
+        setTimeout(function () {
+          done(document.querySelector('.outer .inner .text').innerText)
+        })
+      })).to.equal('Hello from the shadow DOM')
+      expect(await browser.executeAsync(function (done) {
+        setTimeout(function () {
+          done([...document.querySelectorAll('.outer .inner .text')].map(_ => _.innerText))
+        })
+      })).to.deep.equal(['Hello from the shadow DOM'])
+    })
+
+    it('does not affect xpath selectors', async () => {
+      const selector = '//*[@id="hello-world"]'
+      expect(await (await $(selector)).getText()).to.equal('Hello world!')
+      expect(await Promise.all((await $$(selector)).map(_ => _.getText()))).to.deep.equal([
+        'Hello world!'
+      ])
+    })
+
+    it('can pass functions in to $()', async () => {
+      expect(await (await $(function () {
+        return document.querySelector('.text')
+      })).getText()).to.equal('Hello from the shadow DOM')
+      expect(await (await $(() => document.querySelector('.text'))).getText()).to.equal('Hello from the shadow DOM')
+    })
+
+    it('can pass functions in to $$()', async () => {
+      expect(await (await $$(function () {
+        return document.querySelector('.text')
+      }))[0].getText()).to.equal('Hello from the shadow DOM')
+      expect(await (await $$(() => document.querySelector('.text')))[0].getText()).to.equal('Hello from the shadow DOM')
+    })
+
+    it('can query the shadow DOM using foo.querySelector()', async () => {
+      expect(await browser.execute(() => {
+        const body = document.querySelector('body')
+        const outer = body.querySelector('.outer')
+        const inner = outer.querySelector('.inner')
+        const text = inner.querySelector('.text')
+        return text.innerText
+      })).to.equal('Hello from the shadow DOM')
+
+      expect(await browser.execute(() => {
+        const doc = document
+        return doc.querySelector('.text').innerText
+      })).to.equal('Hello from the shadow DOM')
+    })
+
+    it('works with iframes and when changing urls', async () => {
+      expect(await (await $('.text')).getText()).to.equal('Hello from the shadow DOM')
+      await browser.switchToFrame(await $('iframe'))
+      expect(await (await $('.text')).getText()).to.equal('Hello from the other shadow DOM')
+      await browser.url('/othershadowdom.html')
+      expect(await (await $('.text')).getText()).to.equal('Hello from the other shadow DOM')
+    })
+
+    it('recovers from invalid CSS selector', async () => {
+      const fails = async () => {
+        await (await $('.invalid;')).getText()
       }
-    )).to.equal('Hello from the shadow DOM')
-    expect(await
-    browser.execute('return document.querySelector(\'.text\').innerText;')
-    ).to.equal('Hello from the shadow DOM')
-    expect(await browser.execute(
-      function (selector) {
-        return document.querySelector(selector).innerText
-      },
-      '.text'
-    )).to.equal('Hello from the shadow DOM')
-    expect(await browser.execute(
-      'return document.querySelector(arguments[0]).innerText;',
-      '.text'
-    )).to.equal('Hello from the shadow DOM')
-    expect(await browser.execute(
-      () => document.querySelector('.text').innerText
-    )).to.equal('Hello from the shadow DOM')
-    expect(await browser.execute(
-      selector => document.querySelector(selector).innerText,
-      '.text'
-    )).to.equal('Hello from the shadow DOM')
+      await expect(fails()).to.be.rejectedWith(Error)
+    })
   })
 
-  it('can use executeAsync()', async () => {
-    expect(await browser.executeAsync(function (done) {
-      setTimeout(function () {
-        done(document.querySelector('.outer .inner .text').innerText)
-      })
-    })).to.equal('Hello from the shadow DOM')
-    expect(await browser.executeAsync(function (done) {
-      setTimeout(function () {
-        done([...document.querySelectorAll('.outer .inner .text')].map(_ => _.innerText))
-      })
-    })).to.deep.equal(['Hello from the shadow DOM'])
+  describe('frozen window', () => {
+    beforeEach(async () => {
+      await browser.url('/frozenwindow.html')
+    })
+
+    it('recovers when it is impossible to write to window.__kagekiri__', async () => {
+      expect(await (await $('span')).getText()).to.equal('find me!')
+    })
   })
 
-  it('does not affect xpath selectors', async () => {
-    const selector = '//*[@id="hello-world"]'
-    expect(await (await $(selector)).getText()).to.equal('Hello world!')
-    expect(await Promise.all((await $$(selector)).map(_ => _.getText()))).to.deep.equal([
-      'Hello world!'
-    ])
-  })
+  describe('autorefreshes', () => {
+    beforeEach(async () => {
+      await browser.url('/autorefreshes.html')
+    })
 
-  it('can pass functions in to $()', async () => {
-    expect(await (await $(function () {
-      return document.querySelector('.text')
-    })).getText()).to.equal('Hello from the shadow DOM')
-    expect(await (await $(() => document.querySelector('.text'))).getText()).to.equal('Hello from the shadow DOM')
-  })
-
-  it('can pass functions in to $$()', async () => {
-    expect(await (await $$(function () {
-      return document.querySelector('.text')
-    }))[0].getText()).to.equal('Hello from the shadow DOM')
-    expect(await (await $$(() => document.querySelector('.text')))[0].getText()).to.equal('Hello from the shadow DOM')
-  })
-
-  it('can query the shadow DOM using foo.querySelector()', async () => {
-    expect(await browser.execute(() => {
-      const body = document.querySelector('body')
-      const outer = body.querySelector('.outer')
-      const inner = outer.querySelector('.inner')
-      const text = inner.querySelector('.text')
-      return text.innerText
-    })).to.equal('Hello from the shadow DOM')
-
-    expect(await browser.execute(() => {
-      const doc = document
-      return doc.querySelector('.text').innerText
-    })).to.equal('Hello from the shadow DOM')
-  })
-
-  it('works with iframes and when changing urls', async () => {
-    expect(await (await $('.text')).getText()).to.equal('Hello from the shadow DOM')
-    await browser.switchToFrame(await $('iframe'))
-    expect(await (await $('.text')).getText()).to.equal('Hello from the other shadow DOM')
-    await browser.url('/othershadowdom.html')
-    expect(await (await $('.text')).getText()).to.equal('Hello from the other shadow DOM')
+    it('recovers when the page redirects after writing to window.__kagekiri__', async () => {
+      const span = await $('span')
+      await span.waitForExist()
+      expect(await span.getText()).to.equal('find me!')
+    })
   })
 })
